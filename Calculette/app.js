75a3ef22a01a9ccc8408846d7b61434ce11d93c4
Renaud <rzell57@gmail.com>
2023-04-10 17:09:15 +0200

    calc is ok, could do improvements (see comms), planning to redo js from scratch

--- Calculette/app.js
+++ Calculette/app.js
@@ -4,6 +4,14 @@
     let equal = document.querySelector(".btn-equal");
     let clear = document.querySelector(".btn-clear");
     let ops = document.querySelectorAll(".ops");
+    let notAllowedFirst = document.querySelectorAll(".notAllowedFirst");
+    // let res = false;
+
+    //à faire :
+    // après un calcul, empêcher de rentrer un nombre après le résultat mais autoriser uniquement un opérateur via la variable res
+    // empêcher de sasir / et * si la chaîne est vide
+    // boucle for pour vérifier le nombre de . dans value, s'il apparait plus d'une fois, impossible de saisir
+    //refaire les fonctions pour éviter les contradictions
 
     buttons.forEach(function(button){
         button.addEventListener("click", function(e){   
@@ -26,7 +34,20 @@
         }
         screen.value += value;
     })
-});
+    });
+
+    notAllowedFirst.forEach(function(button){
+        button.addEventListener("click", function(e){
+            let value;
+            if(screen.value === ""){
+                value = "";
+            }else{
+                value = e.target.dataset.num
+            }
+        screen.value += value;
+        })
+    })
+
 
     equal.addEventListener("click", function(e){
         if(screen.value === ""){
@@ -35,6 +56,7 @@
             let answer = eval(screen.value);
             screen.value = answer;
         }
+        // res = true;
     });
 
     clear.addEventListener("click", function(e){
