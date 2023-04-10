3ba0140c53c1fe69c321add6fe05f9c14e3f4715
Renaud <rzell57@gmail.com>
2023-04-10 16:06:02 +0200

    Fix répétition opérateurs

--- Calculette/app.js
+++ Calculette/app.js
@@ -1,15 +1,32 @@
 (function(){
-    let screen = document.querySelector(".screen")
-    let buttons = document.querySelectorAll(".btn")
-    let equal = document.querySelector(".btn-equal")
-    let clear = document.querySelector(".btn-clear")
+    let screen = document.querySelector(".screen");
+    let buttons = document.querySelectorAll(".btn");
+    let equal = document.querySelector(".btn-equal");
+    let clear = document.querySelector(".btn-clear");
+    let ops = document.querySelectorAll(".ops");
 
     buttons.forEach(function(button){
-        button.addEventListener("click", function(e){
-            let value = e.target.dataset.num;
-            screen.value += value;
+        button.addEventListener("click", function(e){   
+
+        value = e.target.dataset.num;
+        screen.value += value;
         })
     });
+
+    ops.forEach(function(button){
+        button.addEventListener("click", function(e){
+            if(screen.value[screen.value.length - 1] === "+" ||
+            screen.value[screen.value.length - 1] === "-" ||
+            screen.value[screen.value.length - 1] === "/" ||
+            screen.value[screen.value.length - 1] === "*"||
+            screen.value[screen.value.length - 1] === "."){
+            value = "";
+        }else{
+            value = e.target.dataset.num;
+        }
+        screen.value += value;
+    })
+});
 
     equal.addEventListener("click", function(e){
         if(screen.value === ""){
